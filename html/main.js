d2827f0431dc683b929f0953367c164cb2ac6e57
thelindat <65407488+thelindat@users.noreply.github.com>
2021-01-21 02:37:17 +1100

    Update main.js
    Added support for two description lines.
    Notification height adjustment for number of lines.

--- html/main.js
+++ html/main.js
@@ -15,7 +15,8 @@ $('document').ready(function() {
 
     function ShowNotif(data) {
         var $notification = CreateNotification(data);
-        $('.notif-container').prepend($notification.animate({width: '350px', height:'68px', 'line-height':'1.5em', fontSize: '12px', margin:'0 0 4px 0', opacity:'1'}));
+        var $notifHeight = $notification.height
+        $('.notif-container').prepend($notification.animate({width: '350px', height: ''+ $notifHeight +'', 'line-height':'1.5em', fontSize: '12px', margin:'0 0 4px 0', opacity:'1'}));
         setTimeout(function() {
             $.when($notification.animate({width: '0px', height:'0px', fontSize: '0px', margin:'0 0 0 350px', opacity:'0'})).done(function() {
                 $notification.remove()
@@ -34,20 +35,36 @@ $('document').ready(function() {
                 <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
                 <div id="alert-info"><i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
                 </div>');
+                $notification.height = '50px';
         } else {
             var desc = data.info["desc"];
             if(!/^[A-Za-z0-9]/.test(desc)) {
                 var right = desc.search("\\]");
                 desc = '<span id="descbanner">' + desc.substring(1, right) + '</span> ' + desc.slice(right+2) + ''
             }
-            $notification.html('\
-            <div class="content">\
-            <div id="code">' + data.info["code"] + '</div>\
-            <div id="alert-name">' + data.info["name"] + '</div>\
-            <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
-            <div id="alert-info"><i class="fas fa-info-circle"></i>' + desc + '</br>\
-            <i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
-            </div>');
+            if (data.info["desc2"] == undefined) {
+                $notification.html('\
+                <div class="content">\
+                <div id="code">' + data.info["code"] + '</div>\
+                <div id="alert-name">' + data.info["name"] + '</div>\
+                <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
+                <div id="alert-info"><i class="fas fa-info-circle"></i>' + desc + '</br>\
+                <i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
+                </div>');
+                $notification.height = '68px';
+            } else {
+                var desc2 = data.info["desc2"];
+                $notification.html('\
+                <div class="content">\
+                <div id="code">' + data.info["code"] + '</div>\
+                <div id="alert-name">' + data.info["name"] + '</div>\
+                <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
+                <div id="alert-info"><i class="fas fa-info-circle"></i>' + desc + '</br>\
+                ' + desc2 + '<br>\
+                <i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
+                </div>');
+                $notification.height = '90px';
+            }
         }       
 
         $notification.fadeIn();
