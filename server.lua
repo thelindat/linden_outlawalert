6d796cd078b3f0078c2e105c130c02df2f86e0e4
thelindat <65407488+thelindat@users.noreply.github.com>
2021-01-21 02:34:32 +1100

    Update server.lua
    Removed 911 commands (added to client instead).
    Added Server Callback for character name and phone number.

--- server.lua
+++ server.lua
@@ -1,32 +1,6 @@
 ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-Citizen.CreateThread(function()
-	local mdt = GetResourceState('mdt')
-	if GetResourceState('mdt') == 'started' or GetResourceState('mdt') == 'starting' then
-		RegisterCommand('911', function(playerId, args, rawCommand)
-			if playerId ~= 0 then
-				args = table.concat(args, ' ')
-				local xPlayer = ESX.GetPlayerFromId(playerId)
-				local coords = xPlayer.getCoords()
-				if config.phone_number then caller = getNumberPhone(xPlayer.identifier) else caller = xPlayer.getName() end
-				TriggerEvent('mdt:newCall', args, caller, vector3(coords.x, coords.y, coords.z))
-			end
-		end, false)
-
-		RegisterCommand('911a', function(playerId, args, rawCommand)
-			if playerId ~= 0 then
-				args = table.concat(args, ' ')
-				local xPlayer = ESX.GetPlayerFromId(playerId)
-				local coords = xPlayer.getCoords()
-				TriggerEvent('mdt:newCall', args, "Unknown", vector3(coords.x, coords.y, coords.z))
-			end
-		end, false)
-	else
-		print("Resource 'MDT' is "..mdt)
-	end
-end)
-
 RegisterServerEvent("mdt:newCall")
 AddEventHandler("mdt:newCall", function(details, caller, coords, data)
 	if data then
@@ -41,14 +15,13 @@ AddEventHandler("mdt:newCall", function(details, caller, coords, data)
 	end
 end)
 
--- Get phone number
-function getNumberPhone(identifier)
-    local result = MySQL.Sync.fetchAll("SELECT users.phone_number FROM users WHERE users.identifier = @identifier", {
-        ['@identifier'] = identifier
-    })
-    if result[1] ~= nil then
-        return result[1].phone_number
-    end
-    return nil
-end
-------------------------------------------------------------------------
+ESX.RegisterServerCallback('mdt_outlawalert:getCharData', function(source, cb)
+	local xPlayer = ESX.GetPlayerFromId(source)
+	if not xPlayer then return end
+
+	MySQL.Async.fetchAll('SELECT firstname, lastname, phone_number FROM users WHERE identifier = @identifier', {
+		['@identifier'] = xPlayer.identifier
+	}, function(results)
+		cb(results[1])
+	end)
+end)
