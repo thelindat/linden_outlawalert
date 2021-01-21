efb740c100fac6dd909a59702f8c4443639e3344
thelindat <65407488+thelindat@users.noreply.github.com>
2021-01-21 12:51:05 +1100

    Update server.lua

--- server.lua
+++ server.lua
@@ -19,9 +19,27 @@ ESX.RegisterServerCallback('mdt_outlawalert:getCharData', function(source, cb)
 	local xPlayer = ESX.GetPlayerFromId(source)
 	if not xPlayer then return end
 
-	MySQL.Async.fetchAll('SELECT firstname, lastname, phone_number FROM users WHERE identifier = @identifier', {
-		['@identifier'] = xPlayer.identifier
-	}, function(results)
-		cb(results[1])
+	local PrimaryIdentifier = 'fivem'
+	for k,v in ipairs(GetPlayerIdentifiers(source)) do
+        if string.match(v, PrimaryIdentifier) then
+            local identifier = v
+            MySQL.Async.fetchAll('SELECT firstname, lastname, phone_number FROM users WHERE identifier = @identifier', {
+				['@identifier'] = identifier
+			}, function(results)
+				cb(results[1])
+			end)
+        end
+    end
+end)
+
+ESX.RegisterServerCallback('mdt_outlawalert:isVehicleOwned', function(source, cb, plate)
+	MySQL.Async.fetchAll('SELECT plate FROM owned_vehicles WHERE plate = @plate', {
+		['@plate'] = plate
+	}, function(result)
+		if result[1] then
+			cb(true)
+		else
+			cb(false)
+		end
 	end)
 end)
