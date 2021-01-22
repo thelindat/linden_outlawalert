96ea5511f4aec596fbb6998ff29363f9145d030d
thelindat <65407488+thelindat@users.noreply.github.com>
2021-01-22 22:30:07 +1100

    2.0.0

--- html/main.js
+++ html/main.js
@@ -1,80 +1,43 @@
 $('document').ready(function() {
     alerts = {};
-
-    $(".notification").fadeIn();
-
+    
     window.addEventListener('message', function (event) {
-        if (event.data.action == "display") {
-            ShowNotif(event.data);
-        } else if (event.data.action == "log"){
-            $(".notification").hide();
-        }  else {
-            $(".notification").fadeIn();
-        }
+        ShowNotif(event.data);
     });
 
     function ShowNotif(data) {
-        var $notification = CreateNotification(data);
-        var $notifHeight = $notification.height
-        $('.notif-container').prepend($notification.animate({width: '350px', height: ''+ $notifHeight +'', 'line-height':'1.5em', fontSize: '12px', margin:'0 0 4px 0', opacity:'1'}));
-        setTimeout(function() {
-            $.when($notification.animate({width: '0px', height:'0px', fontSize: '0px', margin:'0 0 0 350px', opacity:'0'})).done(function() {
-                $notification.remove()
-            });
-        }, data.length != null ? data.length : 4500);
-}
-
-    function CreateNotification(data) {
-        var $notification = $(document.createElement('div'));
-        $notification.addClass('notification').addClass(data.info["style"]);
-        if (data.info["desc"] == undefined) {
-            $notification.html('\
-                <div class="content">\
-                <div id="code">' + data.info["code"] + '</div>\
-                <div id="alert-name">' + data.info["name"] + '</div>\
-                <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
-                <div id="alert-info"><i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
-                </div>');
-                $notification.height = '50px';
-        } else {
-            var desc = data.info["desc"];
-            if(!/^[A-Za-z0-9]/.test(desc)) {
-                var right = desc.search("\\]");
-                desc = '<span id="descbanner">' + desc.substring(1, right) + '</span> ' + desc.slice(right+2) + ''
-            }
-            if (data.info["desc2"] == undefined) {
-                $notification.html('\
-                <div class="content">\
-                <div id="code">' + data.info["code"] + '</div>\
-                <div id="alert-name">' + data.info["name"] + '</div>\
-                <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
-                <div id="alert-info"><i class="fas fa-info-circle"></i>' + desc + '</br>\
-                <i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
-                </div>');
-                $notification.height = '68px';
-            } else {
-                var desc2 = data.info["desc2"];
-                $notification.html('\
-                <div class="content">\
-                <div id="code">' + data.info["code"] + '</div>\
-                <div id="alert-name">' + data.info["name"] + '</div>\
-                <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
-                <div id="alert-info"><i class="fas fa-info-circle"></i>' + desc + '</br>\
-                ' + desc2 + '<br>\
-                <i class="fas fa-globe"></i>' + data.info["loc"] + '</div>\
-                </div>');
-                $notification.height = '90px';
-            }
-        }       
-
-        $notification.fadeIn();
-        if (data.info["style"] !== undefined) {
-            Object.keys(data.info["style"]).forEach(function(css) {
-                $notification.css(css, data.info["style"][css])
-            });
-        }
-        return $notification;
+            var $notification = CreateNotification(data);
+            var $height = '75px';
+            $('.notif-container').prepend($notification.animate({width: '350px', height: ''+ $height +'', 'line-height':'1.5em', fontSize: '12px', margin:'0 0 4px 0', opacity:'1'}));
+            setTimeout(function() {
+                $.when($notification.animate({width: '0', height:'0', fontSize: '0', margin:'5px 5px 5px 350px', opacity:'0'})).done(function() {
+                    $notification.remove()
+                });
+            }, data.length != null ? data.length : 2500);
     }
 
+    function CreateNotification(data) {
+       var $notification = $(document.createElement('div'));
+       if (data.info.isImportant === 1) {
+            $notification.addClass('notification').addClass('officer-down');
+       } else {
+            $notification.addClass('notification').addClass(data.job);
+       }
+       $notification.html('\
+       <div class="content">\
+       <div id="code">' + data.info.displayCode + '</div>\
+       <div id="alert-name">' + data.info.dispatchMessage + '</div>\
+       <div id="marker"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></div>\
+       <div id="alert-info"><i class="fas fa-globe-europe"></i>' + data.info.street + '</div>\
+       </div>');
+       $notification.fadeIn();
+       if (data.style !== undefined) {
+           Object.keys(data.style).forEach(function(css) {
+               $notification.css(css, data.style[css])
+           });
+       }
+       return $notification;
+    }
 });
 
+//       <div id="alert-info"><i class="fas fa-globe-europe"></i>' + data.info["loc"] + '</div>\
