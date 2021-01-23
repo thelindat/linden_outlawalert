9729761e4da42a74d8dabdf136066e5d3685ab73
thelindat <65407488+thelindat@users.noreply.github.com>
2021-01-24 06:11:22 +1100

    2.3.0

--- client.lua
+++ client.lua
@@ -1,5 +1,5 @@
 local speedlimitValues = {["Joshua Rd"]=90, ["East Joshua Road"]=90, ["Marina Dr"]=70, ["Alhambra Dr"]=70, ["Niland Ave"]=70, ["Zancudo Ave"]=70, ["Armadillo Ave"]=70, ["Algonquin Blvd"]=70, ["Mountain View Dr"]=70, ["Cholla Springs Ave"]=70, ["Panorama Dr"]=70, ["Lesbos Ln"]=70, ["Calafia Rd"]=70, ["North Calafia Way"]=70, ["Cassidy Trail"]=70, ["Seaview Rd"]=70, ["Grapeseed Main St"]=70, ["Grapeseed Ave"]=70, ["Joad Ln"]=70, ["Union Rd"]=70, ["O'Neil Way"]=70, ["Senora Fwy"]=120, ["Catfish View"]=70, ["Great Ocean Hwy"]=70, ["Paleto Blvd"]=70, ["Duluoz Ave"]=70, ["Procopio Dr"]=70, ["Cascabel Ave"]=70, ["Procopio Promenade"]=70, ["Pyrite Ave"]=70, ["Fort Zancudo Approach Rd"]=70, ["Barbareno Rd"]=70, ["Ineseno Road"]=70, ["West Eclipse Blvd"]=70, ["Playa Vista"]=70, ["Bay City Ave"]=70, ["Del Perro Fwy"]=120, ["Equality Way"]=70, ["Red Desert Ave"]=70, ["Magellan Ave"]=70, ["Sandcastle Way"]=70, ["Vespucci Blvd"]=70, ["Prosperity St"]=70, ["San Andreas Ave"]=70, ["North Rockford Dr"]=70, ["South Rockford Dr"]=70, ["Marathon Ave"]=70, ["Boulevard Del Perro"]=70, ["Cougar Ave"]=70, ["Liberty St"]=70, ["Bay City Incline"]=70, ["Conquistador St"]=70, ["Cortes St"]=70, ["Vitus St"]=70, ["Aguja St"]=70, ["Goma St"]=70, ["Melanoma St"]=70, ["Palomino Ave"]=70, ["Invention Ct"]=70, ["Imagination Ct"]=70, ["Rub St"]=70, ["Tug St"]=70, ["Ginger St"]=70, ["Lindsay Circus"]=70, ["Calais Ave"]=70, ["Adam's Apple Blvd"]=70, ["Alta St"]=70, ["Integrity Way"]=70, ["Swiss St"]=70, ["Strawberry Ave"]=70, ["Capital Blvd"]=70, ["Crusade Rd"]=70, ["Innocence Blvd"]=70, ["Davis Ave"]=70, ["Little Bighorn Ave"]=70, ["Roy Lowenstein Blvd"]=70, ["Jamestown St"]=70, ["Carson Ave"]=45, ["Grove St"]=70, ["Brouge Ave"]=70, ["Covenant Ave"]=70, ["Dutch London St"]=70, ["Signal St"]=70, ["Elysian Fields Fwy"]=120, ["Plaice Pl"]=70, ["Chum St"]=70, ["Chupacabra St"]=70, ["Miriam Turner Overpass"]=70, ["Autopia Pkwy"]=70, ["Exceptionalists Way"]=70, ["La Puerta Fwy"]=120, ["New Empire Way"]=70, ["Runway1"]="--", ["Greenwich Pkwy"]=70, ["Kortz Dr"]=70, ["Banham Canyon Dr"]=70, ["Buen Vino Rd"]=70, ["Route 68"]=120, ["Zancudo Grande Valley"]=70, ["Zancudo Barranca"]=70, ["Galileo Rd"]=70, ["Mt Vinewood Dr"]=70, ["Marlowe Dr"]=70, ["Milton Rd"]=70, ["Kimble Hill Dr"]=70, ["Normandy Dr"]=70, ["Hillcrest Ave"]=70, ["Hillcrest Ridge Access Rd"]=70, ["North Sheldon Ave"]=70, ["Lake Vinewood Dr"]=70, ["Lake Vinewood Est"]=70, ["Baytree Canyon Rd"]=70, ["Peaceful St"]=70, ["North Conker Ave"]=70, ["Wild Oats Dr"]=70, ["Whispymound Dr"]=70, ["Didion Dr"]=70, ["Cox Way"]=70, ["Picture Perfect Drive"]=70, ["South Mo Milton Dr"]=70, ["Cockingend Dr"]=70, ["Mad Wayne Thunder Dr"]=70, ["Hangman Ave"]=70, ["Dunstable Ln"]=70, ["Dunstable Dr"]=70, ["Greenwich Way"]=70, ["Greenwich Pl"]=70, ["Hardy Way"]=70, ["Richman St"]=70, ["Ace Jones Dr"]=70, ["Los Santos Freeway"]=120, ["Senora Rd"]=70, ["Nowhere Rd"]=35, ["Smoke Tree Rd"]=70, ["Cholla Rd"]=70, ["Cat-Claw Ave"]=70, ["Senora Way"]=70, ["Palomino Fwy"]=120, ["Shank St"]=70, ["Macdonald St"]=70, ["Route 68 Approach"]=120, ["Vinewood Park Dr"]=70, ["Vinewood Blvd"]=70, ["Mirror Park Blvd"]=70, ["Glory Way"]=70, ["Bridge St"]=70, ["West Mirror Drive"]=70, ["Nikola Ave"]=70, ["East Mirror Dr"]=70, ["Nikola Pl"]=35, ["Mirror Pl"]=70, ["El Rancho Blvd"]=70, ["Olympic Fwy"]=120, ["Fudge Ln"]=70, ["Amarillo Vista"]=70, ["Labor Pl"]=70, ["El Burro Blvd"]=70, ["Sustancia Rd"]=55, ["South Shambles St"]=70, ["Hanger Way"]=70, ["Orchardville Ave"]=70, ["Popular St"]=70, ["Buccaneer Way"]=55, ["Abattoir Ave"]=70, ["Voodoo Place"]=40, ["Mutiny Rd"]=70, ["South Arsenal St"]=70, ["Forum Dr"]=70, ["Morningwood Blvd"]=70, ["Dorset Dr"]=70, ["Caesars Place"]=70, ["Spanish Ave"]=70, ["Portola Dr"]=70, ["Edwood Way"]=70, ["San Vitus Blvd"]=70, ["Eclipse Blvd"]=70, ["Gentry Lane"]=40, ["Las Lagunas Blvd"]=70, ["Power St"]=70, ["Mt Haan Rd"]=70, ["Elgin Ave"]=70, ["Hawick Ave"]=70, ["Meteor St"]=70, ["Alta Pl"]=70, ["Occupation Ave"]=70, ["Carcer Way"]=70, ["Eastbourne Way"]=70, ["Rockford Dr"]=70, ["Abe Milton Pkwy"]=70, ["Laguna Pl"]=70, ["Sinners Passage"]=70, ["Atlee St"]=70, ["Sinner St"]=70, ["Supply St"]=70, ["Amarillo Way"]=70, ["Tower Way"]=70, ["Decker St"]=70, ["Tackle St"]=70, ["Low Power St"]=70, ["Clinton Ave"]=70, ["Fenwell Pl"]=70, ["Utopia Gardens"]=70, ["Cavalry Blvd"]=70, ["South Boulevard Del Perro"]=70, ["Americano Way"]=70, ["Sam Austin Dr"]=70, ["East Galileo Ave"]=70, ["Galileo Park"]=70, ["West Galileo Ave"]=70, ["Tongva Dr"]=70, ["Zancudo Rd"]=70, ["Movie Star Way"]=70, ["Heritage Way"]=70, ["Perth St"]=70, ["Chianski Passage"]=70, ["Lolita Ave"]=70, ["Meringue Ln"]=70, ["Strangeways Dr"]=70}
-local currentStreetName, intersectStreetName, lastStreet, updateStreet, speedlimit = nil, nil, nil, 0, 0
+local currentStreetName, intersectStreetName, lastStreet, speedlimit = nil, nil, nil, 0
 local isPlayerWhitelisted, playerPed, playerCoords, job, rank, firstname, lastname, phone
 
 function getSpeed() return speedlimit end
@@ -36,22 +36,18 @@ AddEventHandler('esx:setJob', function(job)
 end)
 -- ESX Framework Stuff ---------------------------------------------------------------
 
-function GetTheStreet()
-    local x, y, z = table.unpack(GetEntityCoords(GetPlayerPed(-1), true))
-    local currentStreetHash--[[, intersectStreetHash]] = GetStreetNameAtCoord(x, y, z, currentStreetHash--[[, intersectStreetHash]])
-    currentStreetName = GetStreetNameFromHashKey(currentStreetHash)
-    --intersectStreetName = GetStreetNameFromHashKey(intersectStreetHash)
-    zone = GetLabelText(GetNameOfZone(x, y, z))
-    playerStreetsLocation = zone
-
-    if intersectStreetName ~= nil and intersectStreetName ~= "" then
-        playerStreetsLocation = currentStreetName .. " and " .. intersectStreetName .. ", " .. zone
-    elseif currentStreetName ~= nil and currentStreetName ~= "" then
-        playerStreetsLocation = currentStreetName .. ", " .. zone
-    else
-        playerStreetsLocation = zone
+Citizen.CreateThread(function()
+    while not Config.Loaded do Citizen.Wait(0) end
+    while true do
+        Citizen.Wait(0)
+        if not playerCoords then playerCoords = GetEntityCoords(PlayerPedId()) end
+        if currentStreetName then lastStreet = currentStreetName end
+        local currentStreetHash = GetStreetNameAtCoord(playerCoords.x, playerCoords.y, playerCoords.z)
+        currentStreetName = GetStreetNameFromHashKey(currentStreetHash)
+        if currentStreetName ~= lastStreet and GetVehiclePedIsUsing(playerPed) then speedlimit = speedlimitValues[currentStreetName] end
+        Citizen.Wait(500)
     end
-end
+end)
 
 function refreshPlayerWhitelisted()
 	if not ESX.PlayerData then return false end
@@ -76,8 +72,12 @@ end
 
 function zoneChance(type, playerCoords, street, zoneMod)
     local zone, zoneid, sendit = GetLabelText(GetNameOfZone(playerCoords.x, playerCoords.y, playerCoords.z)), GetZoneAtCoords(playerCoords.x, playerCoords.y, playerCoords.z), false
-    if Config.DebugChance then print('Sending it') return true else
 
+    if Config.DebugChance then
+        pedLocation(zone)
+        print('Sending it')
+        return true
+    else
         if zoneMod == nil then zoneMod = 2 end
         if type == 'Speeding' then zoneMod = zoneMod * 2 end
         local zoneScum = GetZoneScumminess(zoneid)
@@ -110,6 +110,8 @@ function zoneChance(type, playerCoords, street, zoneMod)
             if Config.Debug then print(zoneMod.. ' ' ..zone.. ' ' ..type) end
             sendit = true
         end
+
+        pedLocation(zone)
         return sendit
     end
 end
@@ -117,26 +119,10 @@ end
 function vehicleData(vehicle)
     local vData = {}
     local vehicleClass = GetVehicleClass(vehicle)
-    if vehicleClass == 0 then vehicleClass = _U('compact')
-    elseif vehicleClass == 1 then vehicleClass = _U('sedan')
-    elseif vehicleClass == 2 then vehicleClass = _U('suv')
-    elseif vehicleClass == 3 then vehicleClass = _U('coupe')
-    elseif vehicleClass == 4 then vehicleClass = _U('muscle')
-    elseif vehicleClass == 5 then vehicleClass = _U('sports_classic')
-    elseif vehicleClass == 6 then vehicleClass = _U('sports')
-    elseif vehicleClass == 7 then vehicleClass = _U('super')
-    elseif vehicleClass == 8 then vehicleClass = _U('motorcycle')
-    elseif vehicleClass == 9 then vehicleClass = _U('offroad')
-    elseif vehicleClass == 10 then vehicleClass = _U('industrial')
-    elseif vehicleClass == 11 then vehicleClass = _U('utility')
-    elseif vehicleClass == 12 then vehicleClass = _U('van')
-    elseif vehicleClass == 13 then vehicleClass = 'dnr' -- bicycle
-    elseif vehicleClass == 17 then vehicleClass = _U('service')
-    elseif vehicleClass == 18 then vehicleClass = 'dnr' -- emergency
-    elseif vehicleClass == 19 then vehicleClass = _U('military')
-    elseif vehicleClass == 20 then vehicleClass = _U('truck')
-    end
-    if vehicleClass ~= 'dnr' then
+    local vClass = {[0] = _U('compact'), [1] = _U('sedan'), [2] = _U('suv'), [3] = _U('coupe'), [4] = _U('muscle'), [5] = _U('sports_classic'), [6] = _U('sports'), [7] = _U('super'), [8] = _U('motorcycle'), [9] = _U('offroad'), [10] = _U('industrial'), [11] = _U('utility'), [12] = _U('van'), [17] = _U('service'), [19] = _U('military'), [20] = _U('truck')}
+    local vehicleClass = vClass[vehicleClass]
+    print(vehicleClass)
+    if vehicleClass then
         local vehicleName = GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle)))
         local vehicleColour1, vehicleColour2 = GetVehicleColours(vehicle)
         if vehicleColour1 then
@@ -158,6 +144,15 @@ function vehicleData(vehicle)
         elseif vehicleDoors == 4 then vehicleDoors = _U('four_door') end
         vData.class, vData.name, vData.colour, vData.doors, vData.plate, vData.id = vehicleClass, vehicleName, vehicleColour, vehicleDoors, plate, NetworkGetNetworkIdFromEntity(vehicle)
         return vData
+    end
+end
+
+function pedLocation(zone)
+    playerStreetsLocation = zone
+    if currentStreetName ~= nil and currentStreetName ~= "" then
+        playerStreetsLocation = currentStreetName .. ", " .. zone
+    else
+        playerStreetsLocation = zone
     end
 end
 
@@ -204,54 +199,50 @@ AddEventHandler('wf-alerts:clNotify', function(pData)
 end)
 
 Citizen.CreateThread(function()
-    local sleep = 100
-    Citizen.Wait(1000)
-	while Config.Loaded do
+    while not Config.Loaded do Citizen.Wait(0) end
+	while true do
         Citizen.Wait(0)
+        local sleep = 100
         playerPed = PlayerPedId()
         playerCoords = GetEntityCoords(playerPed)
-        if updateStreet == 0 then GetTheStreet() end
-        if updateStreet == 3 then lastStreet = currentStreetName updateStreet = 0 else updateStreet = updateStreet + 1 end
-        if currentStreetName ~= lastStreet then speedlimit = speedlimitValues[currentStreetName] end
-
         if (not isPlayerWhitelisted or Config.Debug) then
             for k, v in pairs(Config.Timer) do
                 if v > 0 then Config.Timer[k] = v - 1 end
             end
-            if Config.Timer['Shooting'] == 0 and IsPedArmed(playerPed, 4) then sleep = 20 else sleep = 100 end
-
-            if GetVehiclePedIsUsing(playerPed) ~= 0 and not IsPedInAnyHeli(playerPed) and not IsPedInAnyPlane(playerPed) and not IsPedInAnyTrain(playerPed) and not IsPedInAnyBoat(playerPed) then
+            if GetVehiclePedIsUsing(playerPed) ~= 0 then
                 local vehicle = GetVehiclePedIsUsing(playerPed, true)
-                local veh = vehicleData(GetVehiclePedIsUsing(playerPed, true))
-                if veh then
-                    if Config.Timer['Shooting'] == 0 then
-                        if IsPedShooting(playerPed) and not IsPedCurrentWeaponSilenced(playerPed) and IsPedArmed(playerPed, 4) and not BlacklistedWeapon(playerPed) then
-                            if zoneChance('Shooting', playerCoords, currentStreetName) then
-                                data = {dispatchCode = 'driveby', caller = _U('caller_local'), street = playerStreetsLocation, coords = playerCoords, netId = veh.id, length = 6000,
-                                info = ('[%s] %s %s'):format(veh.plate, veh.doors, veh.class), info2 = veh.colour}
-                                TriggerServerEvent('wf-alerts:svNotify', data)
-                                Config.Timer['Shooting'] = Config.Shooting.Success
-                                sleep = 100
-                            else
-                                Config.Timer['Shooting'] = Config.Shooting.Fail
-                                sleep = 100
-                            end
+                if vehicle then
+                    if Config.Timer['Shooting'] == 0 and not BlacklistedWeapon(playerPed) and IsPedShooting(playerPed) and not IsPedCurrentWeaponSilenced(playerPed) and IsPedArmed(playerPed, 4) then
+                        sleep = 50
+                        if zoneChance('Shooting', playerCoords, currentStreetName) then
+                            local veh = vehicleData(vehicle)
+                            data = {dispatchCode = 'driveby', caller = _U('caller_local'), street = playerStreetsLocation, coords = playerCoords, netId = veh.id, length = 6000,
+                            info = ('[%s] %s %s'):format(veh.plate, veh.doors, veh.class), info2 = veh.colour}
+                            TriggerServerEvent('wf-alerts:svNotify', data)
+                            Config.Timer['Shooting'] = Config.Shooting.Success
+                        else
+                            Config.Timer['Shooting'] = Config.Shooting.Fail
                         end
                     end    
-                    if Config.Timer['Speeding'] == 0 and ((GetEntitySpeed(vehicle) * 3.6) >= (speedlimit + (math.random(40,80)))) then
-                        if zoneChance('Speeding', playerCoords, currentStreetName) then
-                            if IsPedInAnyVehicle(playerPed, true) and ((GetEntitySpeed(vehicle) * 3.6) >= (speedlimit + (math.random(30,60)))) then
-                                playerCoords = GetEntityCoords(playerPed)
-                                data = {dispatchCode = 'speeding', caller = _U('caller_local'), street = playerStreetsLocation, coords = playerCoords, netId = veh.id,
-                                info = ('[%s] %s %s'):format(veh.plate, veh.doors, veh.class), info2 = veh.colour}
-                                TriggerServerEvent('wf-alerts:svNotify', data)
-                                Config.Timer['Speeding'] = Config.Speeding.Success
+                    if Config.Timer['Speeding'] == 0 then
+                        if (GetEntitySpeed(vehicle) * 3.6) >= (speedlimit + (math.random(40,80))) then
+                            if zoneChance('Speeding', playerCoords, currentStreetName) then
+                                Citizen.Wait(800)
+                                if IsPedInAnyVehicle(playerPed, true) and ((GetEntitySpeed(vehicle) * 3.6) >= (speedlimit + (math.random(30,60)))) then
+                                    local veh = vehicleData(vehicle)
+                                    playerCoords = GetEntityCoords(playerPed)
+                                    data = {dispatchCode = 'speeding', caller = _U('caller_local'), street = playerStreetsLocation, coords = playerCoords, netId = veh.id,
+                                    info = ('[%s] %s %s'):format(veh.plate, veh.doors, veh.class), info2 = veh.colour}
+                                    TriggerServerEvent('wf-alerts:svNotify', data)
+                                    Config.Timer['Speeding'] = Config.Speeding.Success
+                                end
+                            else
+                                Config.Timer['Speeding'] = Config.Speeding.Fail
                             end
-                        else
-                            Config.Timer['Speeding'] = Config.Speeding.Fail
                         end
                     end
-                    if Config.Timer['Autotheft'] == 0 and (IsPedTryingToEnterALockedVehicle(playerPed) or IsPedJacking(playerPed)) then
+                    if Config.Timer['Autotheft'] == 0 and (IsPedTryingToEnterALockedVehicle(playerPed) or IsPedJacking(playerPed) or (IsPedGettingIntoAVehicle(playerPed) and GetSeatPedIsTryingToEnter(playerPed) == -1 and not IsPedAPlayer(GetPedInVehicleSeat(vehicle, -1)))) then
+                        local veh = vehicleData(vehicle)
                         ESX.TriggerServerCallback('linden_outlawalert:isVehicleOwned', function(hasowner) veh.owned = hasowner end, veh.plate)
                         if not veh.owned then
                             if zoneChance('Autotheft', playerCoords, currentStreetName) then
@@ -266,7 +257,8 @@ Citizen.CreateThread(function()
                     end
                 end
             else -- Any non-vehicle related crimes go below here
-                if Config.Timer['Shooting'] == 0 and IsPedShooting(playerPed) and not IsPedCurrentWeaponSilenced(playerPed) and IsPedArmed(playerPed, 4) and not BlacklistedWeapon(playerPed) then
+                if Config.Timer['Shooting'] == 0 and not BlacklistedWeapon(playerPed) and IsPedShooting(playerPed) and not IsPedCurrentWeaponSilenced(playerPed) and IsPedArmed(playerPed, 4) then
+                    sleep = 50
                     if zoneChance('Shooting', playerCoords, currentStreetName) then
                         data = {dispatchCode = 'shooting', caller = _U('caller_local'), street = playerStreetsLocation, coords = playerCoords, netId = NetworkGetNetworkIdFromEntity(playerPed), length = 6000}
                         TriggerServerEvent('wf-alerts:svNotify', data)
@@ -294,7 +286,6 @@ AddEventHandler('esx:onPlayerDeath', function(reason)
     if isPlayerWhitelisted or Config.Debug then
         local netId = NetworkGetNetworkIdFromEntity(playerPed)
         local name = ('%s %s'):format(rank, lastname)
-        GetTheStreet()
         Citizen.Wait(2000)
         data = {dispatchCode = 'officerdown', caller = name, street = playerStreetsLocation, coords = playerCoords, netId = netId, info = name}
         TriggerServerEvent('wf-alerts:svNotify', data)
