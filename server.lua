d0395bc78e7e409b6a1b26acde1ec772f72871fe
thelindat <65407488+thelindat@users.noreply.github.com>
2021-02-20 13:53:14 +1100

    Update server.lua
    Use `GetResourceMetadata` to get resource version instead of parsing the file

--- server.lua
+++ server.lua
@@ -124,34 +124,26 @@ end)
 
 -- VERSION CHECK
 CreateThread(function()
-    local resourceName, currentVersion, latestVersion = GetCurrentResourceName()
-    local resourceManifest = LoadResourceFile(resourceName, 'fxmanifest.lua')
+    local resourceName = GetCurrentResourceName()
+    local currentVersion, latestVersion = GetResourceMetadata(resourceName, 'version')
     local outdated = '^7[%s]^3 Version ^2%s^3 is available! You are using version ^1%s^7'
-    Citizen.Wait(3000)
+    Citizen.Wait(2000)
     while Config.CheckVersion do
         Citizen.Wait(0)
-        if currentVersion == nil then
-            local data, version = tostring(resourceManifest)
-            for line in data:gmatch("([^\n]*)\n?") do
-                if line:find('^version') then version = line:sub(10, (line:len(line) - 1)) break end
+        PerformHttpRequest("https://raw.githubusercontent.com/thelindat/linden_outlawalert/master/fxmanifest.lua", function (errorCode, resultData, resultHeaders)
+            if errorCode ~= 200 then print("Returned error code:" .. tostring(errorCode)) else
+                local data, version = tostring(resultData)
+                for line in data:gmatch("([^\n]*)\n?") do
+                    if line:find('^version') then version = line:sub(10, (line:len(line) - 1)) break end
+                end         
+                latestVersion = version
             end
-            currentVersion = version
-        else
-            PerformHttpRequest("https://raw.githubusercontent.com/thelindat/linden_outlawalert/master/fxmanifest.lua", function (errorCode, resultData, resultHeaders)
-                if errorCode ~= 200 then print("Returned error code:" .. tostring(errorCode)) else
-                    local data, version = tostring(resultData)
-                    for line in data:gmatch("([^\n]*)\n?") do
-                        if line:find('^version') then version = line:sub(10, (line:len(line) - 1)) break end
-                    end         
-                    latestVersion = version
-                end
-            end)
-            if latestVersion then 
-                if currentVersion ~= latestVersion then
-                    print(outdated:format(resourceName, latestVersion, currentVersion))
-                end
-                Citizen.Wait(60000*Config.CheckVersionDelay)
+        end)
+        if latestVersion then 
+            if currentVersion ~= latestVersion then
+                print(outdated:format(resourceName, latestVersion, currentVersion))
             end
+            Citizen.Wait(60000*Config.CheckVersionDelay)
         end
     end
 end)
