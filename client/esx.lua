5ca5f0ba1ac5634347bcbeb2f59d76d9f9cf872e
thelindat <65407488+thelindat@users.noreply.github.com>
2021-03-27 07:35:39 +1100

    Update esx.lua

--- client/esx.lua
+++ client/esx.lua
@@ -9,18 +9,14 @@ Citizen.CreateThread(function()
 	while ESX.GetPlayerData().job == nil do
 		Citizen.Wait(10)
 	end
-    ESX.PlayerData = ESX.GetPlayerData()
-    job = ESX.PlayerData.job.name
-    rank = ESX.PlayerData.job.grade_label
-    isPlayerWhitelisted = refreshPlayerWhitelisted()
+	ESX.PlayerData = ESX.GetPlayerData()
+	GetPlayerInfo()
+end)
 
-    ESX.TriggerServerCallback('linden_outlawalert:getCharData', function(chardata)
-        firstname = chardata.firstname
-        lastname = chardata.lastname
-        phone = chardata.phone_number
-        if firstname == nil then Citizen.Wait(1000) end
-    end)
-
+RegisterNetEvent('esx:playerLoaded')
+AddEventHandler('esx:playerLoaded', function(playerData)
+	ESX.PlayerData = playerData
+	GetPlayerInfo()
 end)
 
 RegisterNetEvent('esx:setJob')
@@ -30,3 +26,15 @@ AddEventHandler('esx:setJob', function(job)
     rank = ESX.PlayerData.job.grade_label
     isPlayerWhitelisted = refreshPlayerWhitelisted()
 end)
+
+function GetPlayerInfo()
+	ESX.TriggerServerCallback('linden_outlawalert:getCharData', function(chardata)
+        firstname = chardata.firstname
+        lastname = chardata.lastname
+        phone = chardata.phone_number
+        if firstname == nil then Citizen.Wait(1000) end
+    end)
+	job = ESX.PlayerData.job.name
+    rank = ESX.PlayerData.job.grade_label
+    isPlayerWhitelisted = refreshPlayerWhitelisted()
+end
