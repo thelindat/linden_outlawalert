78f9df2977e21f2839b411789775b438ec79cb8a
Linden <65407488+thelindat@users.noreply.github.com>
2021-05-08 21:40:24 +1000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-version '2.6.4'
+version '2.7.0'
 description 'https://github.com/thelindat/linden_outlawalert'
 versioncheck 'https://raw.githubusercontent.com/thelindat/linden_outlawalert/master/fxmanifest.lua'
 
