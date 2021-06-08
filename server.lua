a28b0ab65b1a4a9fed4063a26566cf697c263fc8
Linden <65407488+thelindat@users.noreply.github.com>
2021-06-08 20:38:24 +1000

    Remove version checking

--- server.lua
+++ server.lua
@@ -153,29 +153,3 @@ AddEventHandler('wf-alerts:svNotify911', function(message, caller, coords)
 		TriggerEvent('mdt:newCall', message, caller, vector3(coords.x, coords.y, coords.z), false)
 	end
 end)
-
--- VERSION CHECK
-CreateThread(function()
-	local resourceName = GetCurrentResourceName()
-	local currentVersion, latestVersion = GetResourceMetadata(resourceName, 'version')
-	local outdated = '^6[%s]^3 Version ^2%s^3 is available! You are using version ^1%s^7'
-	Citizen.Wait(2000)
-	while Config.CheckVersion do
-		Citizen.Wait(0)
-		PerformHttpRequest(GetResourceMetadata(resourceName, 'versioncheck'), function (errorCode, resultData, resultHeaders)
-			if errorCode ~= 200 then print("Returned error code:" .. tostring(errorCode)) else
-				local data, version = tostring(resultData)
-				for line in data:gmatch("([^\n]*)\n?") do
-					if line:find('^version') then version = line:sub(10, (line:len(line) - 1)) break end
-				end		 
-				latestVersion = version
-			end
-		end)
-		if latestVersion then 
-			if currentVersion ~= latestVersion then
-				print(outdated:format(resourceName, latestVersion, currentVersion))
-			end
-			Citizen.Wait(60000*Config.CheckVersionDelay)
-		end
-	end
-end)
